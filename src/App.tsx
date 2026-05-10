61a999ed72ede90810a50c7810d3beb17b1e6bfa
Georgiy <scherbinin.zhora2017@yandex.ru>
2026-05-10 16:56:54 +0300

    joke 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <h1>Здравствуйте, Большой Белый Господин.</h1>
       <p>Мы работаем, честно</p>
-      <img src='mts.jpg' width={500} height={300} />
+      <img src='mts.jpg' />
     </div>
   )
 }
